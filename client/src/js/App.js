43fc5aa3a0e49f884030c58c452325d19faecbfa
streamside <t.streamside@gmail.com>
2017-12-16 14:37:06 +0100

    React-router

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import Word from './pages/Word';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom';
+import NewWord from './pages/NewWord';
+import MyWords from './pages/MyWords'
 import '../css/App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <Word />
+      <Router>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/user/word">Nytt Ord</Link>
+          <Link to="/user/mywords">Mina Ord</Link>
+
+          <Switch>
+            <Route exact path="/" render={() => <h1>Home</h1>} />
+            <Route path="/user/word" component={NewWord} />
+            <Route path="/user/mywords" component={MyWords} />
+            <Route render={() => <h1>Page not found</h1>} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/js/pages/MyWords.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MyWords extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Mina Ord</h1>
+      </div>
+    )
+  }
+}
+
+export default MyWords;
